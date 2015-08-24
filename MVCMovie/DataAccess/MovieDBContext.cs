bbd380e92b447a40e60984fe4c0ad1074cd56bc0
Antonio <eltony162@gmail.com>
2015-08-23 20:49:13 -0500

    add

---
+++ MVCMovie/DataAccess/MovieDBContext.cs
@@ -0,0 +1,15 @@
+﻿using MVCMovie.Models;
+using System.Data.Entity;
+
+namespace MVCMovie.DataAccess
+{
+    public class MovieDBContext : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+
+        public MovieDBContext() : base("MovieDBContext")
+        {
+
+        }
+    }
+}
